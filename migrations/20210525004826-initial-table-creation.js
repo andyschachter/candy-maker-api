91e199d50d69f87503c06e9113191b19e299acf8
Andy Schachter <andy.schachter@gmail.com>
2021-05-24 21:07:51 -0400

    added config files, added initial table generation migration

---
+++ migrations/20210525004826-initial-table-creation.js
@@ -0,0 +1,35 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('manufacturers', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      name: { type: Sequelize.STRING, allowNull: false },
+      country: { type: Sequelize.STRING },
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP') 
+      },
+      deletedAt: { type: Sequelize.DATE }
+    })
+
+    return queryInterface.createTable('products', {
+      id: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
+      name: { type: Sequelize.STRING, allowNull: false },
+      yearIntroduced: { type: Sequelize.DATE },
+      manufacturerId: { type: Sequelize.INTEGER, references: { model: 'Manufacturers', key: 'id' } },
+      createdAt: { type: Sequelize.DATE, defaultValue: Sequelize.literal('CURRENT_TIMESTAMP') },
+      updatedAt: {
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP')
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('manufacturers')
+
+    return queryInterface.dropTable('products')
+  }
+};
